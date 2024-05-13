f6a65f6513a9a048d9027c7d0da299cc3dfe94ab
Amir Karimi <amirdev2006@gmail.com>
2024-05-12 22:20:42 -0700

    run it

---
+++ src/js/app.js
@@ -0,0 +1,108 @@
+let number1 = "0";
+let number2 = "";
+let lastAction = "";
+
+
+const screen = document.getElementsByClassName("screen")[0];
+
+function calculator(_key) {
+    switch (_key) {
+        case '+':
+        case '-':
+        case '÷':
+        case 'x':
+            CheckIsSecondNumberEnterAndHandleIt(_key);
+            break;
+        case '=':
+            HandleEqual(_key);
+            if(isNaN(number1))
+            {
+                console.log("CLEAR")
+                Clear();
+            }
+            screen.innerText = number1;
+            break;
+        case '.':
+            CheckDot(_key);
+            break;
+        default:
+            if (lastAction == "=") {
+                if (window.confirm("hello"))
+                    lastAction = "";
+                else
+                    break;
+            }
+            if (number1 == "0") {
+                number1 = "";
+            }
+            number1 += _key;
+            screen.innerText = number1;
+            break;
+    }
+}
+
+
+
+function Clear() {
+    number1 = "0";
+    number2 = "";
+    lastAction = "";
+    screen.innerText = number1;
+
+    
+}
+function Action(action) {
+    if (action == '+')
+        return Number(number1) + Number(number2);
+    else if (action == '-')
+        return Number(number2) - Number(number1);
+    else if (action == '÷')
+        return Number(number2) / Number(number1);
+    else {
+        return Number(number1) * Number(number2);
+    }
+}
+function CheckIsSecondNumberEnterAndHandleIt(_key) {
+    if(number2 != "")
+        number1 = Action(_key);
+    lastAction = _key;
+    number2 = number1;
+    number1 = "0";
+    screen.innerText = number1;
+}
+function CheckDot(_key) {
+    if (!number1.includes('.')) {
+        number1 += _key;
+        screen.innerText = number1;
+    }
+}
+function HandleEqual(_key) {
+    if(lastAction == "")
+    {
+       
+        return;
+    }
+    number1 = Action(lastAction);
+    number2 = "";
+    lastAction = _key;
+}
+
+
+
+const BTN_DARK  = document.getElementById('btn-dark');
+const BTN_LIGHT = document.getElementById('btn-light');
+const HTML      = document.querySelector('html');
+
+BTN_DARK.addEventListener('click', enableDarkMode);
+BTN_LIGHT.addEventListener('click', enableLightMode);
+
+function enableDarkMode()
+{
+    HTML.classList.add('dark');
+}
+
+function enableLightMode()
+{
+    HTML.classList.remove('dark');
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    './*.html',
+    './src/js/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
